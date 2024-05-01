724df21d52ffaab714b41981f4f4a7bd135764f6
Allen Goodman <allen.goodman@icloud.com>
2024-05-01 12:27:56 -0400

    woof

--- src/beignet/nn/functional/_global_distance_test.py
+++ src/beignet/nn/functional/_global_distance_test.py
@@ -1,19 +1,33 @@
+from typing import Sequence
+
 import torch
+from torch import Tensor
 
 
-def global_distance_test(input, other, mask, cutoffs):
+def global_distance_test(
+    input: Tensor,
+    other: Tensor,
+    mask: Tensor,
+    cutoffs: Sequence[float],
+) -> Tensor:
     n = torch.sum(mask, dim=-1)
 
-    input = input.float()
-    other = other.float()
+    y = input - other
+    y = y**2
+    y = torch.sum(y, dim=-1)
+    y = torch.sqrt(y)
 
-    distances = torch.sqrt(torch.sum((input - other) ** 2, dim=-1))
+    scores = torch.zeros(len(cutoffs))
 
-    scores = []
-
-    for c in cutoffs:
-        score = torch.sum((distances <= c) * mask, dim=-1) / n
-        score = torch.mean(score)
-        scores.append(score)
+    for index, cutoff in enumerate(cutoffs):
+        scores[index] = torch.mean(torch.sum((y <= cutoff) * mask, dim=-1) / n)
 
     return sum(scores) / len(scores)
+
+
+def global_distance_test_ts(p1, p2, mask):
+    return global_distance_test(p1, p2, mask, [1.0, 2.0, 4.0, 8.0])
+
+
+def global_distance_test_ha(p1, p2, mask):
+    return global_distance_test(p1, p2, mask, [0.5, 1.0, 2.0, 4.0])
