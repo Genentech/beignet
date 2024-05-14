6ab038c293b7cffbde1f1242f68340368d419b20
Allen Goodman <allen.goodman@icloud.com>
2024-05-14 13:36:02 -0400

    cleanup

--- src/beignet/io/__init__.py
+++ src/beignet/io/__init__.py
@@ -1,10 +1,4 @@
-from ._download import download, download_and_extract_archive, extract_archive
-from ._load_parquet import load_parquet, load_parquet_to_pandas
-from ._md5 import md5
-from ._parse_s3_path import parse_s3_path
 from ._thread_safe_file import ThreadSafeFile
-from ._verify_checksum import verify_checksum
-from ._verify_integrity import verify_integrity
 
 __all__ = [
     "ThreadSafeFile",
--- src/beignet/io/_verify_checksum.py
+++
@@ -1,5 +0,0 @@
-from ._md5 import md5
-
-
-def verify_checksum(path: str, checksum: str, **kwargs) -> bool:
-    return checksum == md5(path, **kwargs)
