c31ddc492ee669f0c0a160dab8ffd40673ceb0a6
Nathan Frey <ncfrey@users.noreply.github.com>
2024-05-14 16:58:24 -0400

    tests

--- tests/beignet/datasets/test__uniref_dataset.py
+++ tests/beignet/datasets/test__uniref_dataset.py
@@ -4,8 +4,9 @@ from beignet.datasets import UniRef50Dataset
 
 
 class TestUnirefDataset:
-    @mock.patch("beignet.io.download_and_extract_archive", return_value=True)
-    def test_init(self, download_func):
+    @mock.patch("beignet.io._download.download_and_extract_archive", return_value=True)
+    @mock.patch("beignet.datasets.UniRef50Dataset.__init__", return_value=None)
+    def test_init(self, download_func, uniref_init):
         dataset = UniRef50Dataset(root="/tmp/data", download=True)
 
         assert download_func.called_once()
