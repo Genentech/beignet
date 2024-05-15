2cd41c38d70df1e60f44fd1433d52fda8aae95a2
Nathan Frey <ncfrey@users.noreply.github.com>
2024-05-15 10:06:43 -0400

    fix test mock

--- tests/beignet/datasets/test__uniref_dataset.py
+++ tests/beignet/datasets/test__uniref_dataset.py
@@ -4,7 +4,7 @@ from beignet.datasets import UniRef50Dataset
 
 
 class TestUnirefDataset:
-    @mock.patch("beignet.io._download.download_and_extract_archive", return_value=True)
+    @mock.patch("pooch.retrieve", return_value=True)
     @mock.patch("beignet.datasets.UniRef50Dataset.__init__", return_value=None)
     def test_init(self, download_func, uniref_init):
         dataset = UniRef50Dataset(root="/tmp/data", download=True)
