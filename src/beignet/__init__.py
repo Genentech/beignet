e3c7f0887f5a37121c1ea34f51b52ac424e15827
Joseph Kleinhenz <kleinhej@gene.com>
2024-05-29 13:37:22 -0400

    wip beignet.root

--- src/beignet/__init__.py
+++ src/beignet/__init__.py
@@ -311,6 +311,7 @@ from ._random_euler_angle import random_euler_angle
 from ._random_quaternion import random_quaternion
 from ._random_rotation_matrix import random_rotation_matrix
 from ._random_rotation_vector import random_rotation_vector
+from ._root import root
 from ._rotation_matrix_identity import rotation_matrix_identity
 from ._rotation_matrix_magnitude import rotation_matrix_magnitude
 from ._rotation_matrix_mean import rotation_matrix_mean
