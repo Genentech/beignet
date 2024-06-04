f1df9d400ea7be91f360bf06355694a2010255ba
Allen Goodman <allen.goodman@icloud.com>
2024-06-04 17:53:59 -0400

    cleanup

--- src/beignet/special/_dawson_integral_f.py
+++ src/beignet/special/_dawson_integral_f.py
@@ -6,5 +6,5 @@ from torch import Tensor
 from ._error_erfi import error_erfi
 
 
-def dawson_integral_f(z: Tensor) -> Tensor:
-    return math.sqrt(torch.pi) / 2.0 * torch.exp(-torch.square(z)) * error_erfi(z)
+def dawson_integral_f(input: Tensor) -> Tensor:
+    return math.sqrt(torch.pi) / 2.0 * torch.exp(-(input**2)) * error_erfi(input)
