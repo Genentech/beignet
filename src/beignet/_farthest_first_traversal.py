09e5da975c4c362a2069da58d9bf4372a62a6550
Samuel Stanton <ss13641@nyu.edu>
2024-06-10 16:30:15 -0400

    add test for tensor library

--- src/beignet/_farthest_first_traversal.py
+++ src/beignet/_farthest_first_traversal.py
@@ -18,7 +18,7 @@ def farthest_first_traversal(
     If no scores are provided, ties are broken by the library index.
 
     Args:
-        library: A list with N elements or a tensor with shape (N,).
+        library: A list with N elements or a tensor with shape (N, *element_shape).
         distance_fn: A callable that takes two arguments and
             returns a distance between them.
         ranking_scores: A tensor with shape (N,) containing the ranking scores
