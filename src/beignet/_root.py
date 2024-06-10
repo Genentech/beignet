e82b912b8856504daea515da35c30957feef454e
Allen Goodman <allen.goodman@icloud.com>
2024-06-10 09:54:54 -0400

    cleanup

--- src/beignet/_root.py
+++ src/beignet/_root.py
@@ -1,4 +1,4 @@
-from typing import Callable, Literal, Optional
+from typing import Callable, Literal
 
 from torch import Tensor
 
@@ -10,13 +10,46 @@ def root(
     x0: Tensor,
     x1: Tensor,
     *,
-    rtol: Optional[float] = None,
-    atol: Optional[float] = None,
+    rtol: float | None = None,
+    atol: float | None = None,
     maxiter: int = 100,
     method: Literal["chandrupatla", "newton"] = "chandrupatla",
     **kwargs,
 ):
-    r""" """
+    r"""
+    Find a root of a scalar function.
+
+    Parameters
+    ----------
+    func : callable
+        The function whose root is to be found.
+
+    x0 : Tensor
+        The initial guess for the root.
+
+    x1 : Tensor
+        The second initial guess for the root.
+
+    rtol : float, optional
+        The relative tolerance for the root.
+
+    atol : float, optional
+        The absolute tolerance for the root.
+
+    maxiter : int, optional
+        The maximum number of iterations.
+
+    method : {"chandrupatla", "newton"}, optional
+        The method to use.
+
+    kwargs
+        Additional keyword arguments to pass to the solver.
+
+    Returns
+    -------
+    output : Tensor
+        The root of the function.
+    """
     match method:
         case "chandrupatla":
             return beignet.chandrupatla(
@@ -35,6 +68,7 @@ def root(
                 rtol=rtol,
                 atol=atol,
                 maxiter=maxiter,
+                **kwargs,
             )
         case _:
             raise ValueError
