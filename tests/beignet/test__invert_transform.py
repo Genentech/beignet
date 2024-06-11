ad2964ae1c236a56184d5d12eddb07367ba31e57
Allen Goodman <allen.goodman@icloud.com>
2024-06-11 15:00:07 -0400

    test_invert_transform

--- tests/beignet/test__invert_transform.py
+++ tests/beignet/test__invert_transform.py
@@ -1,11 +1,36 @@
 import beignet
+import hypothesis.strategies
 import torch.testing
 
 
-def test_invert_transform():
-    input = torch.randn([32, 3])
+@hypothesis.strategies.composite
+def _strategy(function):
+    input = torch.randn(
+        [
+            function(
+                hypothesis.strategies.integers(
+                    min_value=1,
+                    max_value=8,
+                ),
+            ),
+            3,
+        ]
+    )
 
     transform = torch.randn([3, 3])
+
+    return (
+        (input, transform),
+        beignet.apply_transform(
+            beignet.apply_transform(input, transform),
+            beignet.invert_transform(transform),
+        ),
+    )
+
+
+@hypothesis.given(_strategy())
+def test_invert_transform(data):
+    (input, transform), expected = data
 
     torch.testing.assert_close(
         input,
