28c1474f4db3ee4909228a8742899bdb2bbcff6b
Allen Goodman <allen.goodman@icloud.com>
2024-06-19 19:06:56 -0400

    pytorch

--- src/beignet/polynomial/__vander_nd.py
+++ src/beignet/polynomial/__vander_nd.py
@@ -6,14 +6,15 @@ import numpy
 
 def _vander_nd(func, input, degrees):
     n_dims = len(func)
+
     if n_dims != len(input):
-        raise ValueError(
-            f"Expected {n_dims} dimensions of sample points, got {len(input)}"
-        )
+        raise ValueError
+
     if n_dims != len(degrees):
-        raise ValueError(f"Expected {n_dims} dimensions of degrees, got {len(degrees)}")
+        raise ValueError
+
     if n_dims == 0:
-        raise ValueError("Unable to guess a dtype or shape when no points are given")
+        raise ValueError
 
     input = tuple(numpy.asarray(tuple(input)) + 0.0)
 
--- src/beignet/polynomial/__vander_nd_flat.py
+++ src/beignet/polynomial/__vander_nd_flat.py
@@ -1,6 +1,8 @@
+import numpy
+
 from .__vander_nd import _vander_nd
 
 
 def _vander_nd_flat(vander_fs, points, degrees):
     v = _vander_nd(vander_fs, points, degrees)
-    return v.reshape(v.shape[: -len(degrees)] + (-1,))
+    return numpy.reshape(v, v.shape[: -len(degrees)] + (-1,))
