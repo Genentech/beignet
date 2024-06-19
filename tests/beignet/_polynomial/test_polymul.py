ba5e9bc76462214870d40e9ca438c3c63dbb4240
Allen Goodman <allen.goodman@icloud.com>
2024-06-19 16:06:31 -0400

    pytorch

--- tests/beignet/_polynomial/test_polymul.py
+++ tests/beignet/_polynomial/test_polymul.py
@@ -1,25 +1,24 @@
 import beignet.polynomial
-import beignet.polynomial._polytrim
 import torch
 
 
 def test_polymul():
-    for i in range(5):
-        for j in range(5):
-            tgt = torch.zeros(i + j + 1, dtype=torch.float64)
+    for j in range(5):
+        for k in range(5):
+            output = torch.zeros(j + k + 1, dtype=torch.float64)
 
-            tgt[i + j] += 1
+            output[j + k] = output[j + k] + 1
 
             torch.testing.assert_close(
-                beignet.polynomial._polytrim.polytrim(
+                beignet.polynomial.polytrim(
                     beignet.polynomial.polymul(
-                        torch.tensor([0] * i + [1]),
                         torch.tensor([0] * j + [1]),
+                        torch.tensor([0] * k + [1]),
                     ),
                     tolerance=1e-6,
                 ),
-                beignet.polynomial._polytrim.polytrim(
-                    tgt,
+                beignet.polynomial.polytrim(
+                    output,
                     tolerance=1e-6,
                 ),
             )
