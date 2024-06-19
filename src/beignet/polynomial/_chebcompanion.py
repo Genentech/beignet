bc14e9896cbb64c352a919aa2a150a6ee649c45e
Allen Goodman <allen.goodman@icloud.com>
2024-06-19 17:37:16 -0400

    pytorch

--- src/beignet/polynomial/_chebcompanion.py
+++ src/beignet/polynomial/_chebcompanion.py
@@ -5,20 +5,31 @@ import torch
 from .__as_series import _as_series
 
 
-def chebcompanion(c):
-    [c] = _as_series([c])
-    if len(c) < 2:
-        raise ValueError("Series must have maximum degree of at least 1.")
-    if len(c) == 2:
-        return torch.tensor([[-c[0] / c[1]]])
+def chebcompanion(input):
+    (input,) = _as_series([input])
 
-    n = len(c) - 1
-    mat = torch.zeros((n, n), dtype=c.dtype)
+    if len(input) < 2:
+        raise ValueError
+
+    if len(input) == 2:
+        return torch.tensor([[-input[0] / input[1]]])
+
+    n = len(input) - 1
+
+    output = torch.zeros((n, n), dtype=input.dtype)
+
     scl = torch.tensor([1.0] + [math.sqrt(0.5)] * (n - 1))
-    top = mat.reshape(-1)[1 :: n + 1]
-    bot = mat.reshape(-1)[n :: n + 1]
+
+    top = torch.reshape(output, [-1])[1 :: n + 1]
+
+    bot = torch.reshape(output, [-1])[n :: n + 1]
+
     top[0] = math.sqrt(0.5)
+
     top[1:] = 1 / 2
+
     bot[...] = top
-    mat[:, -1] -= (c[:-1] / c[-1]) * (scl / scl[-1]) * 0.5
-    return mat
+
+    output[:, -1] -= (input[:-1] / input[-1]) * (scl / scl[-1]) * 0.5
+
+    return output
