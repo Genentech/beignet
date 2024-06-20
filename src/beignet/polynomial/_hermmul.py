6c91d42c23350f4391ffd9fa5cc7f311dfde6031
Allen Goodman <allen.goodman@icloud.com>
2024-06-20 12:00:01 -0400

    pytorch

--- src/beignet/polynomial/_hermmul.py
+++ src/beignet/polynomial/_hermmul.py
@@ -1,32 +1,42 @@
+import torch
+
 from .__as_series import _as_series
 from ._hermadd import hermadd
 from ._hermmulx import hermmulx
 from ._hermsub import hermsub
 
 
-def hermmul(c1, c2):
-    [c1, c2] = _as_series([c1, c2])
+def hermmul(input, other):
+    input, other = _as_series([input, other])
 
-    if len(c1) > len(c2):
-        c = c2
-        xs = c1
+    if len(input) > len(other):
+        c = other
+        xs = input
     else:
-        c = c1
-        xs = c2
+        c = input
+        xs = other
 
     if len(c) == 1:
         c0 = c[0] * xs
-        c1 = 0
+        input = 0
     elif len(c) == 2:
         c0 = c[0] * xs
-        c1 = c[1] * xs
+        input = c[1] * xs
     else:
         nd = len(c)
         c0 = c[-2] * xs
-        c1 = c[-1] * xs
+        input = c[-1] * xs
         for i in range(3, len(c) + 1):
             tmp = c0
             nd = nd - 1
-            c0 = hermsub(c[-i] * xs, c1 * (2 * (nd - 1)))
-            c1 = hermadd(tmp, hermmulx(c1) * 2)
-    return hermadd(c0, hermmulx(c1) * 2)
+            c0 = hermsub(c[-i] * xs, input * (2 * (nd - 1)))
+            input = hermadd(tmp, hermmulx(input) * 2)
+
+    input = torch.tensor(input)
+    input = torch.ravel(input)
+
+    output = hermmulx(input) * 2
+
+    output = hermadd(c0, output)
+
+    return output
--- tests/beignet/_polynomial/test_hermmul.py
+++ tests/beignet/_polynomial/test_hermmul.py
@@ -1,20 +1,20 @@
 import beignet.polynomial
 import beignet.polynomial._hermmul
 import beignet.polynomial._hermval
-import numpy
+import torch.testing
 
 
 def test_hermmul():
-    x = numpy.linspace(-3, 3, 100)
+    x = torch.linspace(-3, 3, 100)
 
-    for i in range(5):
-        pol1 = [0] * i + [1]
-        val1 = beignet.polynomial._hermval.hermval(x, pol1)
-        for j in range(5):
-            msg = f"At i={i}, j={j}"
-            pol2 = [0] * j + [1]
-            val2 = beignet.polynomial._hermval.hermval(x, pol2)
-            pol3 = beignet.polynomial._hermmul.hermmul(pol1, pol2)
-            val3 = beignet.polynomial._hermval.hermval(x, pol3)
-            numpy.testing.assert_(len(pol3) == i + j + 1, msg)
-            numpy.testing.assert_almost_equal(val3, val1 * val2, err_msg=msg)
+    for j in range(5):
+        pol1 = torch.tensor([0] * j + [1])
+        val1 = beignet.polynomial.hermval(x, pol1)
+
+        for k in range(5):
+            pol2 = torch.tensor([0] * k + [1])
+            val2 = beignet.polynomial.hermval(x, pol2)
+            pol3 = beignet.polynomial.hermmul(pol1, pol2)
+            val3 = beignet.polynomial.hermval(x, pol3)
+            assert len(pol3) == j + k + 1
+            torch.testing.assert_close(val3, val1 * val2, atol=1e-4, rtol=1e-4)
