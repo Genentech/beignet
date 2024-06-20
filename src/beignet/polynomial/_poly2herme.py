cbf5495749a00593d01fb461a5186ac226b07d3b
Allen Goodman <allen.goodman@icloud.com>
2024-06-20 11:19:42 -0400

    pytorch

--- src/beignet/polynomial/_poly2herme.py
+++ src/beignet/polynomial/_poly2herme.py
@@ -1,12 +1,23 @@
+import torch
+
 from .__as_series import _as_series
 from ._hermeadd import hermeadd
 from ._hermemulx import hermemulx
 
 
 def poly2herme(input):
-    [input] = _as_series([input])
-    deg = len(input) - 1
-    res = 0
-    for i in range(deg, -1, -1):
-        res = hermeadd(hermemulx(res), input[i])
-    return res
+    (input,) = _as_series([input])
+
+    degree = len(input) - 1
+
+    output = torch.tensor([0.0])
+
+    for index in range(degree, -1, -1):
+        output = hermemulx(output)
+
+        output = hermeadd(
+            output,
+            torch.ravel(input[index]),
+        )
+
+    return output
--- tests/beignet/_polynomial/test_poly2herme.py
+++ tests/beignet/_polynomial/test_poly2herme.py
@@ -7,6 +7,8 @@ from .test_polynomial import hermite_e_polynomial_coefficients
 def test_poly2herme():
     for index in range(10):
         torch.testing.assert_close(
-            beignet.polynomial.poly2herme(hermite_e_polynomial_coefficients[index]),
-            torch.tensor([0] * index + [1]),
+            beignet.polynomial.poly2herme(
+                hermite_e_polynomial_coefficients[index],
+            ),
+            torch.tensor([0] * index + [1], dtype=torch.float64),
         )
