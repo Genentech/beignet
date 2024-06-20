77ee97faf3a130b71bdfad436ba0c67f32fb2696
Allen Goodman <allen.goodman@icloud.com>
2024-06-20 10:31:19 -0400

    pytorch

--- tests/beignet/_polynomial/test_polyvander.py
+++ tests/beignet/_polynomial/test_polyvander.py
@@ -1,22 +1,35 @@
 import beignet.polynomial
 import numpy
+import torch.testing
 
 
 def test_polyvander():
     x = numpy.arange(3)
     v = beignet.polynomial.polyvander(x, 3)
     numpy.testing.assert_(v.shape == (3, 4))
-    for i in range(4):
-        numpy.testing.assert_almost_equal(
-            v[..., i], beignet.polynomial.polyval(x, [0] * i + [1])
-        )
-    x = numpy.array([[1, 2], [3, 4], [5, 6]])
-    v = beignet.polynomial.polyvander(x, 3)
-    numpy.testing.assert_(v.shape == (3, 2, 4))
 
-    for i in range(4):
-        numpy.testing.assert_almost_equal(
-            v[..., i], beignet.polynomial.polyval(x, [0] * i + [1])
+    for index in range(4):
+        torch.testing.assert_close(
+            v[..., index],
+            beignet.polynomial.polyval(
+                x,
+                torch.tensor([0] * index + [1]),
+            ),
+        )
+
+    x = numpy.array([[1, 2], [3, 4], [5, 6]])
+
+    v = beignet.polynomial.polyvander(x, 3)
+
+    assert v.shape == (3, 2, 4)
+
+    for index in range(4):
+        torch.testing.assert_close(
+            v[..., index],
+            beignet.polynomial.polyval(
+                x,
+                torch.tensor([0] * index + [1]),
+            ),
         )
 
     numpy.testing.assert_raises(
