664b4bb2b540b712bec71a84bac899edc1e4508e
Allen Goodman <allen.goodman@icloud.com>
2024-06-23 11:37:27 -0400

    tests

--- tests/beignet/_polynomial/test_chebyshev_series_roots.py
+++ tests/beignet/_polynomial/test_chebyshev_series_roots.py
@@ -1,21 +1,34 @@
 import beignet.polynomial
-import numpy
 import torch
 
 
 def test_chebyshev_series_roots():
-    torch.testing.assert_close(beignet.polynomial.chebyshev_series_roots([1]), [])
     torch.testing.assert_close(
-        beignet.polynomial.chebyshev_series_roots([1, 2]), [-0.5]
+        beignet.polynomial.chebyshev_series_roots(
+            torch.tensor([1], dtype=torch.float32),
+        ),
+        torch.tensor([], dtype=torch.float32),
     )
-    for i in range(2, 5):
-        tgt = numpy.linspace(-1, 1, i)
+
+    torch.testing.assert_close(
+        beignet.polynomial.chebyshev_series_roots(
+            torch.tensor([1, 2], dtype=torch.float32),
+        ),
+        torch.tensor([-0.5], dtype=torch.float32),
+    )
+
+    for index in range(2, 5):
         torch.testing.assert_close(
             beignet.polynomial.trim_chebyshev_series(
                 beignet.polynomial.chebyshev_series_roots(
-                    beignet.polynomial._chebfromroots.chebfromroots(tgt)
+                    beignet.polynomial.chebfromroots(
+                        torch.linspace(-1, 1, index),
+                    )
                 ),
                 tolerance=1e-6,
             ),
-            beignet.polynomial.trim_chebyshev_series(tgt, tolerance=1e-6),
+            beignet.polynomial.trim_chebyshev_series(
+                torch.linspace(-1, 1, index),
+                tolerance=1e-6,
+            ),
         )
