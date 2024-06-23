51445495b5929209d796f6a2e177d5048a8a2ea9
Allen Goodman <allen.goodman@icloud.com>
2024-06-23 12:22:29 -0400

    tests

--- src/beignet/polynomial/_add_physicists_hermite_series.py
+++ src/beignet/polynomial/_add_physicists_hermite_series.py
@@ -1,5 +1,7 @@
+from torch import Tensor
+
 from .__add import _add
 
 
-def add_physicists_hermite_series(c1, c2):
-    return _add(c1, c2)
+def add_physicists_hermite_series(input: Tensor, other: Tensor) -> Tensor:
+    return _add(input, other)
