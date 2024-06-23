285f66bfadbfc4fdab220432a250b90946ca0207
Allen Goodman <allen.goodman@icloud.com>
2024-06-23 16:44:12 -0400

    tests

--- tests/beignet/_polynomial/test_laguerre_series_from_roots.py
+++ tests/beignet/_polynomial/test_laguerre_series_from_roots.py
@@ -6,14 +6,14 @@ import torch
 def test_laguerre_series_from_roots():
     torch.testing.assert_close(
         beignet.polynomial.trim_laguerre_series(
-            beignet.polynomial._lagfromroots.laguerre_series_from_roots([]),
+            beignet.polynomial.laguerre_series_from_roots([]),
             tolerance=0.000001,
         ),
         [1],
     )
     for i in range(1, 5):
         roots = numpy.cos(numpy.linspace(-numpy.pi, 0, 2 * i + 1)[1::2])
-        pol = beignet.polynomial._lagfromroots.laguerre_series_from_roots(roots)
+        pol = beignet.polynomial.laguerre_series_from_roots(roots)
         assert len(pol) == i + 1
         torch.testing.assert_close(
             beignet.polynomial.laguerre_series_to_power_series(pol)[-1], 1
