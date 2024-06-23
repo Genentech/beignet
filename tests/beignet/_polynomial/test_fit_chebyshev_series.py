607f63820970006c63684e063cc29010c65a611d
Allen Goodman <allen.goodman@icloud.com>
2024-06-23 01:53:27 -0400

    tests

--- tests/beignet/_polynomial/test_fit_chebyshev_series.py
+++ tests/beignet/_polynomial/test_fit_chebyshev_series.py
@@ -7,7 +7,7 @@ def test_fit_chebyshev_series():
     def f(x):
         return x * (x - 1) * (x - 2)
 
-    def f2(x):
+    def g(x):
         return x**4 + x**2 + 1
 
     numpy.testing.assert_raises(
@@ -64,76 +64,118 @@ def test_fit_chebyshev_series():
         TypeError, beignet.polynomial.fit_chebyshev_series, [1], [1], []
     )
 
-    x = numpy.linspace(0, 2)
-    y = f(x)
-
-    coef3 = beignet.polynomial.fit_chebyshev_series(x, y, 3)
+    coef3 = beignet.polynomial.fit_chebyshev_series(
+        numpy.linspace(0, 2), f(numpy.linspace(0, 2)), 3
+    )
     torch.testing.assert_close(len(coef3), 4)
     torch.testing.assert_close(
-        beignet.polynomial.evaluate_chebyshev_series_1d(x, coef3), y
+        beignet.polynomial.evaluate_chebyshev_series_1d(numpy.linspace(0, 2), coef3),
+        f(numpy.linspace(0, 2)),
     )
-    coef3 = beignet.polynomial.fit_chebyshev_series(x, y, [0, 1, 2, 3])
+    coef3 = beignet.polynomial.fit_chebyshev_series(
+        numpy.linspace(0, 2), f(numpy.linspace(0, 2)), [0, 1, 2, 3]
+    )
     torch.testing.assert_close(len(coef3), 4)
     torch.testing.assert_close(
-        beignet.polynomial.evaluate_chebyshev_series_1d(x, coef3), y
+        beignet.polynomial.evaluate_chebyshev_series_1d(numpy.linspace(0, 2), coef3),
+        f(numpy.linspace(0, 2)),
     )
 
-    coef4 = beignet.polynomial.fit_chebyshev_series(x, y, 4)
-    torch.testing.assert_close(len(coef4), 5)
-    torch.testing.assert_close(
-        beignet.polynomial.evaluate_chebyshev_series_1d(x, coef4), y
+    coef4 = beignet.polynomial.fit_chebyshev_series(
+        numpy.linspace(0, 2), f(numpy.linspace(0, 2)), 4
     )
-    coef4 = beignet.polynomial.fit_chebyshev_series(x, y, [0, 1, 2, 3, 4])
     torch.testing.assert_close(len(coef4), 5)
     torch.testing.assert_close(
-        beignet.polynomial.evaluate_chebyshev_series_1d(x, coef4), y
+        beignet.polynomial.evaluate_chebyshev_series_1d(numpy.linspace(0, 2), coef4),
+        f(numpy.linspace(0, 2)),
+    )
+    coef4 = beignet.polynomial.fit_chebyshev_series(
+        numpy.linspace(0, 2), f(numpy.linspace(0, 2)), [0, 1, 2, 3, 4]
+    )
+    torch.testing.assert_close(len(coef4), 5)
+    torch.testing.assert_close(
+        beignet.polynomial.evaluate_chebyshev_series_1d(numpy.linspace(0, 2), coef4),
+        f(numpy.linspace(0, 2)),
     )
 
-    coef4 = beignet.polynomial.fit_chebyshev_series(x, y, [2, 3, 4, 1, 0])
+    coef4 = beignet.polynomial.fit_chebyshev_series(
+        numpy.linspace(0, 2), f(numpy.linspace(0, 2)), [2, 3, 4, 1, 0]
+    )
     torch.testing.assert_close(len(coef4), 5)
     torch.testing.assert_close(
-        beignet.polynomial.evaluate_chebyshev_series_1d(x, coef4), y
+        beignet.polynomial.evaluate_chebyshev_series_1d(numpy.linspace(0, 2), coef4),
+        f(numpy.linspace(0, 2)),
     )
 
-    coef2d = beignet.polynomial.fit_chebyshev_series(x, numpy.array([y, y]).T, 3)
+    coef2d = beignet.polynomial.fit_chebyshev_series(
+        numpy.linspace(0, 2),
+        numpy.array([(f(numpy.linspace(0, 2))), (f(numpy.linspace(0, 2)))]).T,
+        3,
+    )
     torch.testing.assert_close(coef2d, numpy.array([coef3, coef3]).T)
     coef2d = beignet.polynomial.fit_chebyshev_series(
-        x, numpy.array([y, y]).T, [0, 1, 2, 3]
+        numpy.linspace(0, 2),
+        numpy.array([(f(numpy.linspace(0, 2))), (f(numpy.linspace(0, 2)))]).T,
+        [0, 1, 2, 3],
     )
     torch.testing.assert_close(coef2d, numpy.array([coef3, coef3]).T)
 
-    w = numpy.zeros_like(x)
-    yw = y.copy()
+    w = numpy.zeros_like(numpy.linspace(0, 2))
+    yw = f(numpy.linspace(0, 2)).copy()
     w[1::2] = 1
-    y[0::2] = 0
-    wcoef3 = beignet.polynomial.fit_chebyshev_series(x, yw, 3, w=w)
+    f(numpy.linspace(0, 2))[0::2] = 0
+    wcoef3 = beignet.polynomial.fit_chebyshev_series(numpy.linspace(0, 2), yw, 3, w=w)
     torch.testing.assert_close(wcoef3, coef3)
-    wcoef3 = beignet.polynomial.fit_chebyshev_series(x, yw, [0, 1, 2, 3], w=w)
+    wcoef3 = beignet.polynomial.fit_chebyshev_series(
+        numpy.linspace(0, 2), yw, [0, 1, 2, 3], w=w
+    )
     torch.testing.assert_close(wcoef3, coef3)
 
     wcoef2d = beignet.polynomial.fit_chebyshev_series(
-        x, numpy.array([yw, yw]).T, 3, w=w
+        numpy.linspace(0, 2), numpy.array([yw, yw]).T, 3, w=w
     )
     torch.testing.assert_close(wcoef2d, numpy.array([coef3, coef3]).T)
     wcoef2d = beignet.polynomial.fit_chebyshev_series(
-        x, numpy.array([yw, yw]).T, [0, 1, 2, 3], w=w
+        numpy.linspace(0, 2), numpy.array([yw, yw]).T, [0, 1, 2, 3], w=w
     )
     torch.testing.assert_close(wcoef2d, numpy.array([coef3, coef3]).T)
 
-    x = [1, 1j, -1, -1j]
-    torch.testing.assert_close(beignet.polynomial.fit_chebyshev_series(x, x, 1), [0, 1])
     torch.testing.assert_close(
-        beignet.polynomial.fit_chebyshev_series(x, x, [0, 1]), [0, 1]
+        beignet.polynomial.fit_chebyshev_series([1, 1j, -1, -1j], [1, 1j, -1, -1j], 1),
+        [0, 1],
+    )
+    torch.testing.assert_close(
+        beignet.polynomial.fit_chebyshev_series(
+            [1, 1j, -1, -1j], [1, 1j, -1, -1j], [0, 1]
+        ),
+        [0, 1],
     )
 
-    x = numpy.linspace(-1, 1)
-    y = f2(x)
-    coef1 = beignet.polynomial.fit_chebyshev_series(x, y, 4)
     torch.testing.assert_close(
-        beignet.polynomial.evaluate_chebyshev_series_1d(x, coef1), y
+        beignet.polynomial.evaluate_chebyshev_series_1d(
+            numpy.linspace(-1, 1),
+            beignet.polynomial.fit_chebyshev_series(
+                numpy.linspace(-1, 1), g(numpy.linspace(-1, 1)), 4
+            ),
+        ),
+        g(numpy.linspace(-1, 1)),
     )
-    coef2 = beignet.polynomial.fit_chebyshev_series(x, y, [0, 2, 4])
+
     torch.testing.assert_close(
-        beignet.polynomial.evaluate_chebyshev_series_1d(x, coef2), y
+        beignet.polynomial.evaluate_chebyshev_series_1d(
+            numpy.linspace(-1, 1),
+            beignet.polynomial.fit_chebyshev_series(
+                numpy.linspace(-1, 1), g(numpy.linspace(-1, 1)), [0, 2, 4]
+            ),
+        ),
+        g(numpy.linspace(-1, 1)),
     )
-    torch.testing.assert_close(coef1, coef2)
+
+    torch.testing.assert_close(
+        beignet.polynomial.fit_chebyshev_series(
+            numpy.linspace(-1, 1), g(numpy.linspace(-1, 1)), 4
+        ),
+        beignet.polynomial.fit_chebyshev_series(
+            numpy.linspace(-1, 1), g(numpy.linspace(-1, 1)), [0, 2, 4]
+        ),
+    )
