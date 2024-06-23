d51364ff3af00e13a6b1b537f1c8b025d630325e
Allen Goodman <allen.goodman@icloud.com>
2024-06-23 01:54:37 -0400

    tests

--- tests/beignet/_polynomial/test_hermegrid3d.py
+++ tests/beignet/_polynomial/test_hermegrid3d.py
@@ -9,15 +9,16 @@ def test_hermegrid3d():
     c3d = numpy.einsum("i,j,k->ijk", c1d, c1d, c1d)
 
     x = numpy.random.random((3, 5)) * 2 - 1
-    y = beignet.polynomial._polyval.evaluate_power_series_1d(x, [1.0, 2.0, 3.0])
+    y = beignet.polynomial.evaluate_power_series_1d(x, [1.0, 2.0, 3.0])
 
     x1, x2, x3 = x
     y1, y2, y3 = y
 
-    tgt = numpy.einsum("i,j,k->ijk", y1, y2, y3)
-    res = beignet.polynomial._hermegrid3d.hermegrid3d(x1, x2, x3, c3d)
-    torch.testing.assert_close(res, tgt)
+    torch.testing.assert_close(
+        beignet.polynomial.hermegrid3d(x1, x2, x3, c3d),
+        numpy.einsum("i,j,k->ijk", y1, y2, y3),
+    )
 
     z = numpy.ones((2, 3))
-    res = beignet.polynomial._hermegrid3d.hermegrid3d(z, z, z, c3d)
+    res = beignet.polynomial.hermegrid3d(z, z, z, c3d)
     assert res.shape == (2, 3) * 3
