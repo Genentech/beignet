2512c0972322c32a120e186c18ae710e832e7aef
Allen Goodman <allen.goodman@icloud.com>
2024-06-23 02:01:59 -0400

    tests

--- tests/beignet/_polynomial/test_leggrid2d.py
+++ tests/beignet/_polynomial/test_leggrid2d.py
@@ -13,10 +13,21 @@ def test_leggrid2d():
     x1, x2, x3 = x
     y1, y2, y3 = y
 
-    tgt = torch.einsum("i,j->ij", y1, y2)
-    res = beignet.polynomial.leggrid2d(x1, x2, c2d)
-    torch.testing.assert_close(res, tgt)
+    torch.testing.assert_close(
+        beignet.polynomial.leggrid2d(
+            x1,
+            x2,
+            c2d,
+        ),
+        torch.einsum(
+            "i,j->ij",
+            y1,
+            y2,
+        ),
+    )
 
-    z = torch.ones((2, 3))
+    z = torch.ones([2, 3])
+
     res = beignet.polynomial.leggrid2d(z, z, c2d)
+
     assert res.shape == (2, 3) * 2
