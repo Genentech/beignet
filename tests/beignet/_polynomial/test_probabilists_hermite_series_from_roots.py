f62c7d7a0118823f997d981361a7450b79b0b38a
Allen Goodman <allen.goodman@icloud.com>
2024-06-23 16:23:43 -0400

    tests

--- tests/beignet/_polynomial/test_probabilists_hermite_series_from_roots.py
+++ tests/beignet/_polynomial/test_probabilists_hermite_series_from_roots.py
@@ -1,22 +1,37 @@
 import beignet.polynomial
-import numpy
 import torch
 
 
 def test_probabilists_hermite_series_from_roots():
-    res = beignet.polynomial.probabilists_hermite_series_from_roots([])
+    res = beignet.polynomial.probabilists_hermite_series_from_roots(
+        torch.tensor([]),
+    )
+
     torch.testing.assert_close(
         beignet.polynomial.trim_probabilists_hermite_series(res, tolerance=0.000001),
-        [1],
+        torch.tensor([1], dtype=torch.float32),
     )
-    for i in range(1, 5):
-        roots = numpy.cos(numpy.linspace(-numpy.pi, 0, 2 * i + 1)[1::2])
-        pol = beignet.polynomial.probabilists_hermite_series_from_roots(roots)
-        res = beignet.polynomial.evaluate_probabilists_hermite_series_1d(roots, pol)
+
+    for index in range(1, 5):
+        pol = beignet.polynomial.probabilists_hermite_series_from_roots(
+            torch.cos(torch.linspace(-torch.pi, 0, 2 * index + 1)[1::2]),
+        )
+
+        res = beignet.polynomial.evaluate_probabilists_hermite_series_1d(
+            torch.cos(torch.linspace(-torch.pi, 0, 2 * index + 1)[1::2]),
+            pol,
+        )
+
         tgt = 0
-        assert len(pol) == i + 1
+
+        assert len(pol) == index + 1
+
         torch.testing.assert_close(
             beignet.polynomial.probabilists_hermite_series_to_power_series(pol)[-1],
-            1,
+            torch.tensor(1, dtype=torch.float32),
         )
-        torch.testing.assert_close(res, tgt)
+
+        torch.testing.assert_close(
+            res,
+            torch.tensor(tgt, dtype=torch.float32),
+        )
