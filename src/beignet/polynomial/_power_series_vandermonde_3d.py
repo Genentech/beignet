258e7d37e170e84041e6737931ecf0a3864b76e3
Allen Goodman <allen.goodman@icloud.com>
2024-06-23 12:24:48 -0400

    tests

--- src/beignet/polynomial/_power_series_vandermonde_3d.py
+++ src/beignet/polynomial/_power_series_vandermonde_3d.py
@@ -1,8 +1,10 @@
+from torch import Tensor
+
 from .__vander_nd_flat import _vander_nd_flat
 from ._power_series_vandermonde_1d import power_series_vandermonde_1d
 
 
-def power_series_vandermonde_3d(x, y, z, deg):
+def power_series_vandermonde_3d(x: Tensor, y: Tensor, z: Tensor, deg):
     return _vander_nd_flat(
         (
             power_series_vandermonde_1d,
