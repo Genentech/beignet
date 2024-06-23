a9ff3f855468a34b1113c7fa03b1e6ba0ed42901
Allen Goodman <allen.goodman@icloud.com>
2024-06-22 22:59:02 -0400

    pytorch

--- src/beignet/polynomial/_pow_power_series.py
+++ src/beignet/polynomial/_pow_power_series.py
@@ -1,7 +1,7 @@
-import numpy
+import torchaudio.functional
 
 from .__pow import _pow
 
 
 def pow_power_series(c, pow, maxpower=None):
-    return _pow(numpy.convolve, c, pow, maxpower)
+    return _pow(torchaudio.functional.convolve, c, pow, maxpower)
