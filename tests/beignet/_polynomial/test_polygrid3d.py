ffe4673a392c3fc874b63faa373a44ecf420bc12
Allen Goodman <allen.goodman@icloud.com>
2024-06-23 00:37:01 -0400

    tests

--- tests/beignet/_polynomial/test_polygrid3d.py
+++ tests/beignet/_polynomial/test_polygrid3d.py
@@ -1,21 +1,47 @@
 import beignet.polynomial
-import numpy
+import torch.testing
 
 
 def test_polygrid3d():
-    c1d = numpy.array([1.0, 2.0, 3.0])
-    c3d = numpy.einsum("i,j,k->ijk", c1d, c1d, c1d)
+    x = torch.rand([3, 5]) * 2 - 1
 
-    x = numpy.random.random((3, 5)) * 2 - 1
     x1, x2, x3 = x
-    y1, y2, y3 = beignet.polynomial.evaluate_power_series_1d(x, [1.0, 2.0, 3.0])
 
-    numpy.testing.assert_almost_equal(
-        beignet.polynomial.polygrid3d(x1, x2, x3, c3d),
-        numpy.einsum("i,j,k->ijk", y1, y2, y3),
+    y1, y2, y3 = beignet.polynomial.evaluate_power_series_1d(
+        x,
+        [1.0, 2.0, 3.0],
     )
 
-    z = numpy.ones((2, 3))
-    numpy.testing.assert_(
-        beignet.polynomial.polygrid3d(z, z, z, c3d).shape == (2, 3) * 3
+    torch.testing.assert_close(
+        beignet.polynomial.polygrid3d(
+            x1,
+            x2,
+            x3,
+            torch.einsum(
+                "i,j,k->ijk",
+                torch.tensor([1.0, 2.0, 3.0]),
+                torch.tensor([1.0, 2.0, 3.0]),
+                torch.tensor([1.0, 2.0, 3.0]),
+            ),
+        ),
+        torch.einsum(
+            "i,j,k->ijk",
+            y1,
+            y2,
+            y3,
+        ),
     )
+
+    output = beignet.polynomial.polygrid3d(
+        torch.ones([2, 3]),
+        torch.ones([2, 3]),
+        torch.ones([2, 3]),
+        torch.einsum(
+            "i,j,k->ijk",
+            torch.tensor([1.0, 2.0, 3.0]),
+            torch.tensor([1.0, 2.0, 3.0]),
+            torch.tensor([1.0, 2.0, 3.0]),
+        ),
+    )
+
+    assert output.shape == (2, 3) * 3
