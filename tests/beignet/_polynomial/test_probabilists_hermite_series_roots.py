d303eb102aa56bdcf243b4d47903bd888eb371d3
Allen Goodman <allen.goodman@icloud.com>
2024-06-23 13:00:06 -0400

    tests

--- tests/beignet/_polynomial/test_probabilists_hermite_series_roots.py
+++ tests/beignet/_polynomial/test_probabilists_hermite_series_roots.py
@@ -17,18 +17,18 @@ def test_probabilists_hermite_series_roots():
         torch.tensor([-1], dtype=torch.float64),
     )
 
-    # for index in range(2, 5):
-    #     torch.testing.assert_close(
-    #         beignet.polynomial.trim_probabilists_hermite_series(
-    #             beignet.polynomial.probabilists_hermite_series_roots(
-    #                 beignet.polynomial.probabilists_hermite_series_from_roots(
-    #                     torch.linspace(-1, 1, index),
-    #                 )
-    #             ),
-    #             tolerance=1e-6,
-    #         ),
-    #         beignet.polynomial.trim_probabilists_hermite_series(
-    #             torch.linspace(-1, 1, index),
-    #             tolerance=1e-6,
-    #         ),
-    #     )
+    for index in range(2, 5):
+        torch.testing.assert_close(
+            beignet.polynomial.trim_probabilists_hermite_series(
+                beignet.polynomial.probabilists_hermite_series_roots(
+                    beignet.polynomial.probabilists_hermite_series_from_roots(
+                        torch.linspace(-1, 1, index),
+                    )
+                ),
+                tolerance=1e-6,
+            ),
+            beignet.polynomial.trim_probabilists_hermite_series(
+                torch.linspace(-1, 1, index),
+                tolerance=1e-6,
+            ),
+        )
