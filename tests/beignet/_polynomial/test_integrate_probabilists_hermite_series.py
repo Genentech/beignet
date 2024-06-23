c1734609b7feb41a5ed28f8c0a28a6e22385c8a4
Allen Goodman <allen.goodman@icloud.com>
2024-06-23 01:48:04 -0400

    tests

--- tests/beignet/_polynomial/test_integrate_probabilists_hermite_series.py
+++ tests/beignet/_polynomial/test_integrate_probabilists_hermite_series.py
@@ -44,21 +44,30 @@ def test_integrate_probabilists_hermite_series():
 
     for i in range(2, 5):
         k = [0] * (i - 2) + [1]
-        res = beignet.polynomial.integrate_probabilists_hermite_series([0], m=i, k=k)
-        torch.testing.assert_close(res, [0, 1])
+        torch.testing.assert_close(
+            beignet.polynomial.integrate_probabilists_hermite_series([0], m=i, k=k),
+            [0, 1],
+        )
 
     for i in range(5):
         scl = i + 1
         pol = [0] * i + [1]
-        tgt = [i] + [0] * i + [1 / scl]
-        hermepol = beignet.polynomial.power_series_to_probabilists_hermite_series(pol)
-        hermeint = beignet.polynomial.integrate_probabilists_hermite_series(
-            hermepol, m=1, k=[i]
-        )
-        res = beignet.polynomial.probabilists_hermite_series_to_power_series(hermeint)
         torch.testing.assert_close(
-            beignet.polynomial.trim_probabilists_hermite_series(res, tolerance=1e-6),
-            beignet.polynomial.trim_probabilists_hermite_series(tgt, tolerance=1e-6),
+            beignet.polynomial.trim_probabilists_hermite_series(
+                beignet.polynomial.probabilists_hermite_series_to_power_series(
+                    beignet.polynomial.integrate_probabilists_hermite_series(
+                        beignet.polynomial.power_series_to_probabilists_hermite_series(
+                            pol
+                        ),
+                        m=1,
+                        k=[i],
+                    )
+                ),
+                tolerance=1e-6,
+            ),
+            beignet.polynomial.trim_probabilists_hermite_series(
+                [i] + [0] * i + [1 / scl], tolerance=1e-6
+            ),
         )
 
     for i in range(5):
@@ -77,13 +86,20 @@ def test_integrate_probabilists_hermite_series():
         scl = i + 1
         pol = [0] * i + [1]
         tgt = [i] + [0] * i + [2 / scl]
-        hermepol = beignet.polynomial.power_series_to_probabilists_hermite_series(pol)
-        hermeint = beignet.polynomial.integrate_probabilists_hermite_series(
-            hermepol, m=1, k=[i], scl=2
-        )
-        res = beignet.polynomial.probabilists_hermite_series_to_power_series(hermeint)
         torch.testing.assert_close(
-            beignet.polynomial.trim_probabilists_hermite_series(res, tolerance=1e-6),
+            beignet.polynomial.trim_probabilists_hermite_series(
+                beignet.polynomial.probabilists_hermite_series_to_power_series(
+                    beignet.polynomial.integrate_probabilists_hermite_series(
+                        beignet.polynomial.power_series_to_probabilists_hermite_series(
+                            pol
+                        ),
+                        m=1,
+                        k=[i],
+                        scl=2,
+                    )
+                ),
+                tolerance=1e-6,
+            ),
             beignet.polynomial.trim_probabilists_hermite_series(tgt, tolerance=1e-6),
         )
 
@@ -93,10 +109,10 @@ def test_integrate_probabilists_hermite_series():
             tgt = pol[:]
             for _ in range(j):
                 tgt = beignet.polynomial.integrate_probabilists_hermite_series(tgt, m=1)
-            res = beignet.polynomial.integrate_probabilists_hermite_series(pol, m=j)
             torch.testing.assert_close(
                 beignet.polynomial.trim_probabilists_hermite_series(
-                    res, tolerance=1e-6
+                    beignet.polynomial.integrate_probabilists_hermite_series(pol, m=j),
+                    tolerance=1e-6,
                 ),
                 beignet.polynomial.trim_probabilists_hermite_series(
                     tgt, tolerance=1e-6
@@ -111,12 +127,12 @@ def test_integrate_probabilists_hermite_series():
                 tgt = beignet.polynomial.integrate_probabilists_hermite_series(
                     tgt, m=1, k=[k]
                 )
-            res = beignet.polynomial.integrate_probabilists_hermite_series(
-                pol, m=j, k=list(range(j))
-            )
             torch.testing.assert_close(
                 beignet.polynomial.trim_probabilists_hermite_series(
-                    res, tolerance=1e-6
+                    beignet.polynomial.integrate_probabilists_hermite_series(
+                        pol, m=j, k=list(range(j))
+                    ),
+                    tolerance=1e-6,
                 ),
                 beignet.polynomial.trim_probabilists_hermite_series(
                     tgt, tolerance=1e-6
@@ -131,12 +147,12 @@ def test_integrate_probabilists_hermite_series():
                 tgt = beignet.polynomial.integrate_probabilists_hermite_series(
                     tgt, m=1, k=[k], lbnd=-1
                 )
-            res = beignet.polynomial.integrate_probabilists_hermite_series(
-                pol, m=j, k=list(range(j)), lbnd=-1
-            )
             torch.testing.assert_close(
                 beignet.polynomial.trim_probabilists_hermite_series(
-                    res, tolerance=1e-6
+                    beignet.polynomial.integrate_probabilists_hermite_series(
+                        pol, m=j, k=list(range(j)), lbnd=-1
+                    ),
+                    tolerance=1e-6,
                 ),
                 beignet.polynomial.trim_probabilists_hermite_series(
                     tgt, tolerance=1e-6
@@ -151,12 +167,12 @@ def test_integrate_probabilists_hermite_series():
                 tgt = beignet.polynomial.integrate_probabilists_hermite_series(
                     tgt, m=1, k=[k], scl=2
                 )
-            res = beignet.polynomial.integrate_probabilists_hermite_series(
-                pol, m=j, k=list(range(j)), scl=2
-            )
             torch.testing.assert_close(
                 beignet.polynomial.trim_probabilists_hermite_series(
-                    res, tolerance=1e-6
+                    beignet.polynomial.integrate_probabilists_hermite_series(
+                        pol, m=j, k=list(range(j)), scl=2
+                    ),
+                    tolerance=1e-6,
                 ),
                 beignet.polynomial.trim_probabilists_hermite_series(
                     tgt, tolerance=1e-6
@@ -165,20 +181,26 @@ def test_integrate_probabilists_hermite_series():
 
     c2d = numpy.random.random((3, 4))
 
-    tgt = numpy.vstack(
-        [beignet.polynomial.integrate_probabilists_hermite_series(c) for c in c2d.T]
-    ).T
-    res = beignet.polynomial.integrate_probabilists_hermite_series(c2d, axis=0)
-    torch.testing.assert_close(res, tgt)
-
-    tgt = numpy.vstack(
-        [beignet.polynomial.integrate_probabilists_hermite_series(c) for c in c2d]
+    torch.testing.assert_close(
+        beignet.polynomial.integrate_probabilists_hermite_series(c2d, axis=0),
+        numpy.vstack(
+            [beignet.polynomial.integrate_probabilists_hermite_series(c) for c in c2d.T]
+        ).T,
     )
-    res = beignet.polynomial.integrate_probabilists_hermite_series(c2d, axis=1)
-    torch.testing.assert_close(res, tgt)
 
-    tgt = numpy.vstack(
-        [beignet.polynomial.integrate_probabilists_hermite_series(c, k=3) for c in c2d]
+    torch.testing.assert_close(
+        beignet.polynomial.integrate_probabilists_hermite_series(c2d, axis=1),
+        numpy.vstack(
+            [beignet.polynomial.integrate_probabilists_hermite_series(c) for c in c2d]
+        ),
     )
-    res = beignet.polynomial.integrate_probabilists_hermite_series(c2d, k=3, axis=1)
-    torch.testing.assert_close(res, tgt)
+
+    torch.testing.assert_close(
+        beignet.polynomial.integrate_probabilists_hermite_series(c2d, k=3, axis=1),
+        numpy.vstack(
+            [
+                beignet.polynomial.integrate_probabilists_hermite_series(c, k=3)
+                for c in c2d
+            ]
+        ),
+    )
