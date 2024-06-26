f2b21d6fd7a178604e80a3d8d10184d46ffa1889
Allen Goodman <allen.goodman@icloud.com>
2024-06-26 12:57:27 -0400

    tests

--- src/beignet/polynomial.py
+++ src/beignet/polynomial.py
@@ -1003,11 +1003,13 @@ def herm2poly(c):
             c1 = polyadd(tmp, polymulx(c1, "same") * 2)
             return c0, c1
 
-        b = n - 2
         x = (c0, c1)
+
         y = x
-        for index in range(0, b):
+
+        for index in range(0, n - 2):
             y = body(index, y)
+
         c0, c1 = y
 
         return polyadd(c0, polymulx(c1, "same") * 2)
