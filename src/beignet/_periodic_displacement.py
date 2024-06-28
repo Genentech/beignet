9d624f9bd0251c8d41f0cbe6a4d1cc52a7409b6c
Henry Isaacson <isaacsoh@gene.com>
2024-06-28 14:16:59 -0400

    test

--- src/beignet/_periodic_displacement.py
+++ src/beignet/_periodic_displacement.py
@@ -3,14 +3,16 @@ from torch import Tensor
 
 
 def periodic_displacement(side: Tensor, dR: Tensor) -> Tensor:
-  """Wraps displacement vectors into a hypercube.
+    """Wraps displacement vectors into a hypercube.
 
-  Args:
-    side: Specification of hypercube size. Either,
-      (a) float if all sides have equal length.
-      (b) tensor(spatial_dim) if sides have different lengths.
-    dR: Matrix of displacements; `tensor(shape=[..., spatial_dim])`.
-  Returns:
-    Matrix of wrapped displacements; `tensor(shape=[..., spatial_dim])`.
-  """
-  return torch.remainder(dR + side * torch.tensor(0.5), side) - torch.tensor(0.5) * side
+    Args:
+      side: Specification of hypercube size. Either,
+        (a) float if all sides have equal length.
+        (b) tensor(spatial_dim) if sides have different lengths.
+      dR: Matrix of displacements; `tensor(shape=[..., spatial_dim])`.
+    Returns:
+      Matrix of wrapped displacements; `tensor(shape=[..., spatial_dim])`.
+    """
+    return (
+        torch.remainder(dR + side * torch.tensor(0.5), side) - torch.tensor(0.5) * side
+    )
