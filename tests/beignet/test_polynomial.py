593050306dbe7250a105e1039701d5c5d40bbc2b
Allen Goodman <allen.goodman@icloud.com>
2024-06-29 20:49:11 -0400

    tests

--- tests/beignet/test_polynomial.py
+++ tests/beignet/test_polynomial.py
@@ -5541,30 +5541,47 @@ def test_legfit():
 def test_legfromroots():
     torch.testing.assert_close(
         legtrim(
-            legfromroots(torch.tensor([])),
+            legfromroots(
+                torch.tensor([]),
+            ),
             tol=0.000001,
         ),
-        [1],
+        torch.tensor([1.0]),
     )
-    for i in range(1, 5):
-        assert (
-            legfromroots(torch.cos(torch.linspace(-math.pi, 0, 2 * i + 1)[1::2])).shape[
-                -1
-            ]
-            == i + 1
+    for index in range(1, 5):
+        input = torch.linspace(-math.pi, 0, 2 * index + 1)[1::2]
+
+        output = legfromroots(
+            torch.cos(
+                input,
+            ),
         )
+
+        assert output.shape[-1] == index + 1
+
         torch.testing.assert_close(
             leg2poly(
-                legfromroots(torch.cos(torch.linspace(-math.pi, 0, 2 * i + 1)[1::2]))
+                legfromroots(
+                    torch.cos(
+                        input,
+                    ),
+                )
             )[-1],
-            1,
+            torch.tensor([1.0]),
         )
+
         torch.testing.assert_close(
             legval(
-                torch.cos(torch.linspace(-math.pi, 0, 2 * i + 1)[1::2]),
-                legfromroots(torch.cos(torch.linspace(-math.pi, 0, 2 * i + 1)[1::2])),
+                torch.cos(
+                    input,
+                ),
+                legfromroots(
+                    torch.cos(
+                        input,
+                    ),
+                ),
             ),
-            0,
+            torch.tensor([0.0]),
         )
 
 
