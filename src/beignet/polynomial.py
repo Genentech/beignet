7a653ef02d449b5ffe50b3bd92494c2a0cfbad14
Allen Goodman <allen.goodman@icloud.com>
2024-06-30 00:20:29 -0400

    tests

--- src/beignet/polynomial.py
+++ src/beignet/polynomial.py
@@ -3444,6 +3444,32 @@ def polyfit(
     full: bool = False,
     weight: Tensor | None = None,
 ):
+    r"""
+    Parameters
+    ----------
+    input : Tensor
+        Independent variable.
+
+    other : Tensor
+        Dependent variable.
+
+    degree : Tensor or int
+        Degree of the fitting polynomial.
+
+    relative_condition : float, optional
+        Relative condition number.
+
+    full : bool, default=False
+        Return additional information.
+
+    weight : Tensor, optional
+        Weights.
+
+    Returns
+    -------
+    output : Tensor
+        Polynomial coefficients of the fit.
+    """
     return _fit(
         polyvander,
         input,
