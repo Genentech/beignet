6e910029669d9a18161b8d511743843a4de53e1c
Allen Goodman <allen.goodman@icloud.com>
2024-07-09 13:39:49 -0400

    HDF5TrajectoryDataset

--- src/beignet/datasets/_hdf5_trajectory_dataset.py
+++ src/beignet/datasets/_hdf5_trajectory_dataset.py
@@ -1,8 +1,32 @@
 from os import PathLike
+from typing import Any, Callable
+
+import mdtraj
+from mdtraj import Trajectory
 
 from ._trajectory_dataset import TrajectoryDataset
 
 
 class HDF5TrajectoryDataset(TrajectoryDataset):
-    def __init__(self, root: str | PathLike, *args, **kwargs):
-        super().__init__(root, *args, **kwargs)
+    def __init__(
+        self,
+        root: str | PathLike,
+        transform: Callable[[Trajectory, ...], Any] | None = None,
+        stride: int | None = None,
+        *args,
+        **kwargs,
+    ):
+        super().__init__(root, transform, stride, *args, **kwargs)
+
+        self.paths = self.root.glob("*.hdf5")
+
+    def __getitem__(self, index: int) -> Trajectory:
+        item = mdtraj.load_hdf5(self.paths[index], stride=self.stride)
+
+        if self.transform:
+            item = self.transform(item)
+
+        return item
+
+    def __len__(self):
+        return len(self.paths)
--- src/beignet/datasets/_trajectory_dataset.py
+++ src/beignet/datasets/_trajectory_dataset.py
@@ -1,14 +1,34 @@
 from os import PathLike
 from pathlib import Path
+from typing import Callable
 
 from torch.utils.data import Dataset
 
+from beignet.transforms import Transform
+
 
 class TrajectoryDataset(Dataset):
-    def __init__(self, root: str | PathLike, *args, **kwargs):
+    def __init__(
+        self,
+        root: str | PathLike,
+        transform: Callable | Transform | None = None,
+        stride: int | None = None,
+        *args,
+        **kwargs,
+    ):
         if isinstance(root, str):
             root = Path(root)
 
         self.root = root.resolve()
 
+        self.transform = transform
+
+        self.stride = stride
+
         super().__init__(*args, **kwargs)
+
+    def __getitem__(self, index: int):
+        raise NotImplementedError
+
+    def __len__(self):
+        raise NotImplementedError
