eaa5b75bae994ea79b1eae6d7b5b0cea49d8250a
Joseph Kleinhenz <kleinhej@gene.com>
2024-07-30 16:58:52 -0700

    wip autograd

--- src/beignet/_bisect.py
+++ src/beignet/_bisect.py
@@ -45,3 +45,38 @@ def bisect(
         iterations += ~converged
 
     return c, (converged, iterations)
+
+
+def root_scalar(
+    f: Callable,
+    *args,
+    a: Tensor,
+    b: Tensor,
+    rtol: float | None = None,
+    atol: float | None = None,
+    maxiter: int = 100,
+    **_,
+):
+    class Root(torch.autograd.Function):
+        #        generate_vmap_rule = True # FIXME this doesn't work
+
+        @staticmethod
+        def forward(*args):
+            root, _ = bisect(
+                lambda x: f(x, *args), a, b, rtol=rtol, atol=atol, maxiter=maxiter
+            )
+            return root
+
+        @staticmethod
+        def setup_context(ctx, inputs, output):
+            ctx.save_for_backward(output, *inputs)
+
+        @staticmethod
+        def backward(ctx, *grad_outputs):
+            root, *args = ctx.saved_tensors
+            A, B = torch.func.grad(f, argnums=(0, 1))(
+                root, *args
+            )  # FIXME handle multiple arguments
+            return -grad_outputs[0] * B / A
+
+    return Root.apply(*args)
