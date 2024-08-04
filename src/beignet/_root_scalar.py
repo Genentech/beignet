7a52c2d1e9f09466105c514780427c30198e3e08
Joseph Kleinhenz <kleinhenz.joseph@gmail.com>
2024-08-04 13:33:34 -0700

    add chandrupatla to root_scalar

--- src/beignet/_root_scalar.py
+++ src/beignet/_root_scalar.py
@@ -16,12 +16,14 @@ class RootSolutionInfo:
 def root_scalar(
     f: Callable,
     *args,
-    method: Literal["bisect"] = "bisect",
+    method: Literal["bisect"] | Literal["chandrupatla"] = "chandrupatla",
     implicit_diff: bool = True,
     options: dict,
 ):
     if method == "bisect":
         solver = beignet.bisect
+    elif method == "chandrupatla":
+        solver = beignet.chandrupatla
     else:
         raise ValueError(f"method {method} not recognized")
 
