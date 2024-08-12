f268ec64f0287c9d3aa47fcc75fe2d192d7ca2c5
Allen Goodman <allen.goodman@icloud.com>
2024-08-12 13:34:01 -0400

    cleanup

--- src/beignet/func/_implicit_diff_root_scalar.py
+++ src/beignet/func/_implicit_diff_root_scalar.py
@@ -2,24 +2,25 @@ from typing import Callable
 
 import torch
 from torch import Tensor
+from torch.autograd import Function
 
 
-def implicit_diff_root_scalar(solver: Callable[..., Tensor]):
+def implicit_diff_root_scalar(func: Callable[..., Tensor]):
     """Wrap a scalar root solver in an autograd function.
 
     Parameters
     ----------
-    solver: Callable[..., Tensor]
+    func: Callable[..., Tensor]
         A scalar root solver.
         `solver(f, *args, **kwargs)` should return a root of `f`.
         Gradients can be computed with respect to *args.
     """
 
     def inner(f, *args, **kwargs):
-        class SolverWrapper(torch.autograd.Function):
+        class SolverWrapper(Function):
             @staticmethod
             def forward(*args):
-                return solver(f, *args, **kwargs)
+                return func(f, *args, **kwargs)
 
             @staticmethod
             def setup_context(ctx, inputs, output):
@@ -33,27 +34,33 @@ def implicit_diff_root_scalar(solver: Callable[..., Tensor]):
                 # optimality condition:
                 # f(x^*(theta), theta) = 0
 
-                A, *B = torch.func.jacrev(f, argnums=tuple(range(nargs + 1)))(
-                    xstar, *args
-                )
+                argnums = tuple(range(nargs + 1))
 
-                if A.ndim == 0:
-                    return tuple(
-                        -g * b / A for g, b in zip(grad_outputs, B, strict=True)
-                    )
-                elif A.ndim == 2:
-                    # NOTE even in this case A is diagonal because f is scalar
-                    return tuple(
-                        torch.linalg.solve(A, -g * b)
-                        for g, b in zip(grad_outputs, B, strict=True)
-                    )
-                else:
-                    raise RuntimeError(f"{A.ndim=} != 0 or 2")
+                a, *b = torch.func.jacrev(f, argnums=argnums)(xstar, *args)
+
+                match a.ndim:
+                    case 0:
+                        output = ()
+
+                        for g, b2 in zip(grad_outputs, b, strict=True):
+                            output = (*output, -g * b2 / a)
+
+                        return output
+                    case 2:  # NOTE: `a` is diagonal because `f` is scalar
+                        output = ()
+
+                        for g, b2 in zip(grad_outputs, b, strict=True):
+                            output = (*output, torch.linalg.solve(a, -g * b2))
+
+                        return output
+                    case _:
+                        raise ValueError
 
             @staticmethod
             def vmap(info, in_dims, *args):
                 # vmap is trivial because in scalar case we just broadcast
-                out = solver(f, *args, **kwargs)
+                out = func(f, *args, **kwargs)
+
                 return out, 0
 
         return SolverWrapper.apply(*args)
