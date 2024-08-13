748d799275abf540a527131945f5dbb33c42ae06
Allen Goodman <allen.goodman@icloud.com>
2024-08-13 14:53:01 -0400

    custom_root

---
+++ src/beignet/func/_custom_root.py
@@ -0,0 +1,36 @@
+from typing import Any, Callable, Tuple
+
+import torch
+from torch import Tensor
+from torch.autograd import Function
+
+
+def custom_root(
+    func: Callable[[Tensor], Tensor],
+    x0: Tensor,
+    f: Callable[[Callable, Tensor], Tensor],
+    g: Callable[[Callable, Tensor], Tensor],
+) -> Tensor:
+    class _CustomRoot(Function):
+        @staticmethod
+        def forward(*args: Any, **kwargs: Any) -> Tensor:
+            return f(func, *args, **kwargs)
+
+        @staticmethod
+        def setup_context(ctx: Any, inputs: Tuple[Any, ...], output: Any):
+            (ouput,) = output
+
+            ctx.save_for_backward(ouput)
+
+        @staticmethod
+        def backward(ctx: Any, *grad_outputs: Any) -> Any:
+            (output,) = ctx.saved_tensors
+
+            _, jvp_fn = torch.func.linearize(func, output)
+
+            def fn(x):
+                return jvp_fn(x)
+
+            return g(fn, *grad_outputs)
+
+    return _CustomRoot.apply(x0)
