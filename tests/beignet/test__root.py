7ce12992a80249cc530be52d446027a47189059f
Allen Goodman <allen.goodman@icloud.com>
2024-08-13 14:56:17 -0400

    custom_root

--- tests/beignet/test__root.py
+++ tests/beignet/test__root.py
@@ -5,7 +5,7 @@ import torch
 def test_root():
     c = torch.linspace(2, 100, 1001, dtype=torch.float64)
 
-    output, _ = beignet.root(
+    output = beignet.root(
         lambda x: x**2 - c,
         torch.sqrt(c) - 1.1,
         torch.sqrt(c) + 1.0,
