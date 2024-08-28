2e09fa9349ebb0197456850b8bc1617b4cfe026f
Joseph Kleinhenz <kleinhej@gene.com>
2024-08-28 10:13:25 -0700

    simplify custom_scalar_root + add jacrev test

--- tests/beignet/test__root_scalar.py
+++ tests/beignet/test__root_scalar.py
@@ -44,3 +44,20 @@ def test_root_scalar_grad(method):
     expected = torch.func.vmap(torch.func.grad(xstar))(c)
 
     torch.testing.assert_close(grad, expected)
+
+
+@pytest.mark.parametrize("method", ["bisect", "chandrupatla"])
+def test_root_scalar_jacrev(method):
+    c = torch.linspace(1.0, 10.0, 101, dtype=torch.float64)
+
+    lower = 0.0
+    upper = 5.0
+    options = {"lower": lower, "upper": upper, "dtype": torch.float64}
+
+    jac = torch.func.jacrev(
+        lambda c: beignet.root_scalar(f, c, method=method, options=options)
+    )(c)
+
+    expected = torch.func.vmap(torch.func.grad(xstar))(c)
+
+    torch.testing.assert_close(torch.diag(jac), expected)
