729ae4e9f9345750511f34ad8e0f74b0e874af06
Joseph Kleinhenz <kleinhej@gene.com>
2024-08-28 10:51:42 -0700

    add docstring to root_scalar

--- src/beignet/_root_scalar.py
+++ src/beignet/_root_scalar.py
@@ -16,10 +16,36 @@ class RootSolutionInfo:
 def root_scalar(
     func: Callable,
     *args,
-    method: Literal["bisect"] | Literal["chandrupatla"] = "chandrupatla",
+    method: Literal["bisect", "chandrupatla"] = "chandrupatla",
     implicit_diff: bool = True,
-    options: dict,
+    options: dict | None = None,
 ):
+    """
+    Find the root of a scalar (elementwise) function.
+
+    Parameters
+    ----------
+    func: Callable
+        Function to find a root of. Called as `f(x, *args)`.
+        The function must operate element wise, i.e. `f(x[i]) == f(x)[i]`.
+        Handling *args via broadcasting is acceptable.
+
+    *args
+        Extra arguments to be passed to `func`.
+
+    method: Literal["bisect", "chandrupatla"] = "chandrupatla"
+        Solver method to use.
+
+    implicit_diff: bool = True
+        If true, the solver is wrapped in `beignet.func.custom_scalar_root` which
+        enables gradients with respect to *args using implicit differentiation.
+
+    options: dict | None = None
+        A dictionary of options that are passed through to the solver as keyword args.
+    """
+    if options is None:
+        options = {}
+
     if method == "bisect":
         solver = beignet.bisect
     elif method == "chandrupatla":
