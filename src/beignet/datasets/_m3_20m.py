391675c08b33f66363a7b6826d872c23c85df524
karinazad <karina.zadorozhny@gmail.com>
2025-01-22 14:55:43 -0500

    M3M20 Dataset

---
+++ src/beignet/datasets/_m3_20m.py
@@ -0,0 +1,47 @@
+from pathlib import Path
+from typing import Callable, Tuple, TypeVar
+
+from beignet.transforms import Transform
+
+from ._parquet_dataset import ParquetDataset
+
+T = TypeVar("T")
+
+
+class M320MDataset(ParquetDataset):
+    """Multi-Modal Molecular (M^3) Dataset with 20M compounds.
+
+    Reference: https://arxiv.org/abs/2412.06847
+
+    Contains SMILES and text descriptions.
+    """
+
+    def __init__(
+        self,
+        root: str | Path,
+        *,
+        transform: Callable | Transform | None = None,
+        target_transform: Callable | Transform | None = None,
+    ):
+        super().__init__(
+            root=root,
+            path="",  # TODO: Add path
+            columns=["smiles", "Description"],
+            target_columns=None,
+        )
+
+        self._x = self._data[self._columns].apply(tuple, axis=1)
+
+    def __getitem__(self, index: int) -> Tuple[T, T]:
+        x = self._x[index]
+
+        if len(x) == 1:
+            x = x[0]
+
+        if self.transform is not None:
+            x = self.transform(x)
+
+        return x
+
+    def __len__(self) -> int:
+        return len(self._data)
