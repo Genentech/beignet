c827cc14e2df63a58e400c716a179335cf51ad72
Joseph Kleinhenz <kleinhej@gene.com>
2025-04-23 13:58:18 -0400

    only stack once

--- src/beignet/_quaternion_to_rotation_matrix.py
+++ src/beignet/_quaternion_to_rotation_matrix.py
@@ -17,36 +17,19 @@ def quaternion_to_rotation_matrix(input: Tensor) -> Tensor:
         Rotation matrices.
     """
 
-    input = input / torch.linalg.vector_norm(input, ord=2, dim=-1, keepdim=True)
-
     a, b, c, d = torch.unbind(input, dim=-1)
 
     return torch.stack(
         [
-            torch.stack(
-                [
-                    a.pow(2) - b.pow(2) - c.pow(2) + d.pow(2),
-                    2 * (a * b) - 2 * (c * d),
-                    2 * (a * c) + 2 * (b * d),
-                ],
-                dim=-1,
-            ),
-            torch.stack(
-                [
-                    2 * (a * b) + 2 * (c * d),
-                    -a.pow(2) + b.pow(2) - c.pow(2) + d.pow(2),
-                    2 * (b * c) - 2 * (a * d),
-                ],
-                dim=-1,
-            ),
-            torch.stack(
-                [
-                    2 * (a * c) - 2 * (b * d),
-                    2 * (b * c) + 2 * (a * d),
-                    -a.pow(2) - b.pow(2) + c.pow(2) + d.pow(2),
-                ],
-                dim=-1,
-            ),
+            a.pow(2) - b.pow(2) - c.pow(2) + d.pow(2),
+            2 * (a * b) - 2 * (c * d),
+            2 * (a * c) + 2 * (b * d),
+            2 * (a * b) + 2 * (c * d),
+            -a.pow(2) + b.pow(2) - c.pow(2) + d.pow(2),
+            2 * (b * c) - 2 * (a * d),
+            2 * (a * c) - 2 * (b * d),
+            2 * (b * c) + 2 * (a * d),
+            -a.pow(2) - b.pow(2) + c.pow(2) + d.pow(2),
         ],
-        dim=-2,
-    )
+        dim=-1,
+    ).view(input.shape[:-1] + (3, 3))
