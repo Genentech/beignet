422e0efed68ef3f790cba1ed6e88e872ade11dd7
Joseph Kleinhenz <kleinhej@gene.com>
2025-04-23 13:49:37 -0400

    vectorize quaternion_to_rotation_matrix
    now also supports multiple batch dimensions

--- src/beignet/_quaternion_to_rotation_matrix.py
+++ src/beignet/_quaternion_to_rotation_matrix.py
@@ -16,28 +16,37 @@ def quaternion_to_rotation_matrix(input: Tensor) -> Tensor:
     output : Tensor, shape=(..., 3, 3)
         Rotation matrices.
     """
-    output = torch.empty(
-        [input.shape[0], 3, 3],
-        dtype=input.dtype,
-        layout=input.layout,
-        device=input.device,
+
+    input = input / torch.linalg.vector_norm(input, ord=2, dim=-1, keepdim=True)
+
+    a, b, c, d = torch.unbind(input, dim=-1)
+
+    return torch.stack(
+        [
+            torch.stack(
+                [
+                    a.pow(2) - b.pow(2) - c.pow(2) + d.pow(2),
+                    2 * (a * b) - 2 * (c * d),
+                    2 * (a * c) + 2 * (b * d),
+                ],
+                dim=-1,
+            ),
+            torch.stack(
+                [
+                    2 * (a * b) + 2 * (c * d),
+                    -a.pow(2) + b.pow(2) - c.pow(2) + d.pow(2),
+                    2 * (b * c) - 2 * (a * d),
+                ],
+                dim=-1,
+            ),
+            torch.stack(
+                [
+                    2 * (a * c) - 2 * (b * d),
+                    2 * (b * c) + 2 * (a * d),
+                    -a.pow(2) - b.pow(2) + c.pow(2) + d.pow(2),
+                ],
+                dim=-1,
+            ),
+        ],
+        dim=-2,
     )
-
-    for j in range(input.shape[0]):
-        a = input[j, 0]
-        b = input[j, 1]
-        c = input[j, 2]
-        d = input[j, 3]
-
-        output[j, 0, 0] = +(a**2.0) - b**2.0 - c**2.0 + d**2.0
-        output[j, 1, 1] = -(a**2.0) + b**2.0 - c**2.0 + d**2.0
-        output[j, 2, 2] = -(a**2.0) - b**2.0 + c**2.0 + d**2.0
-
-        output[j, 0, 1] = 2.0 * (a * b) - 2.0 * (c * d)
-        output[j, 0, 2] = 2.0 * (a * c) + 2.0 * (b * d)
-        output[j, 1, 0] = 2.0 * (a * b) + 2.0 * (c * d)
-        output[j, 1, 2] = 2.0 * (b * c) - 2.0 * (a * d)
-        output[j, 2, 0] = 2.0 * (a * c) - 2.0 * (b * d)
-        output[j, 2, 1] = 2.0 * (b * c) + 2.0 * (a * d)
-
-    return output
