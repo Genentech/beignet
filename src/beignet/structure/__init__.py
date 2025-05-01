269f34ac566a84192cdfb6018e161300f6a6602b
Joseph Kleinhenz <kleinhej@gene.com>
2025-05-01 15:15:11 -0400

    add algorithm to rename ambiguous atoms to minimize lddt

--- src/beignet/structure/__init__.py
+++ src/beignet/structure/__init__.py
@@ -1,2 +1,6 @@
 from ._residue_array import ResidueArray
 from ._rigid import Rigid
+from ._swap_ambiguous_atom_thin_atoms import (
+    rename_symmetric_atoms,
+    swap_ambiguous_atom_thin_atoms,
+)
--- src/beignet/structure/_swap_ambiguous_atom_thin_atoms.py
+++ src/beignet/structure/_swap_ambiguous_atom_thin_atoms.py
@@ -16,7 +16,7 @@ _AMBIGUOUS_ATOM_SWAPS = {
 
 
 @cache
-def _make_ambiguous_atom_swap_indices() -> list[list]:
+def _make_ambiguous_atom_swap_indices() -> list[list[int]]:
     out = []
     for res in STANDARD_RESIDUES:
         atoms = ATOM_THIN_ATOMS[AMINO_ACID_1_TO_3[res]]
@@ -29,8 +29,20 @@ def _make_ambiguous_atom_swap_indices() -> list[list]:
     return out
 
 
+@cache
+def _make_atom_thin_is_ambiguous_mask() -> list[int]:
+    out = []
+    for res in STANDARD_RESIDUES:
+        atoms = ATOM_THIN_ATOMS[AMINO_ACID_1_TO_3[res]]
+        res_swaps = _AMBIGUOUS_ATOM_SWAPS.get(AMINO_ACID_1_TO_3[res], {})
+        res_swaps = res_swaps | dict((v, k) for k, v in res_swaps.items())
+        mask = [atom in res_swaps for atom in atoms]
+        out.append(mask)
+    return out
+
+
 def swap_ambiguous_atom_thin_atoms(
-    atom_thin_xyz: Tensor, residue_type: Tensor, atom_thin_mask: Tensor | None = None
+    residue_type: Tensor, atom_thin_xyz: Tensor, atom_thin_mask: Tensor | None = None
 ) -> tuple[Tensor, Tensor | None]:
     ambiguous_swap_indices = torch.as_tensor(
         _make_ambiguous_atom_swap_indices(), device=residue_type.device
@@ -52,3 +64,73 @@ def swap_ambiguous_atom_thin_atoms(
         )
 
     return atom_thin_xyz, atom_thin_mask
+
+
+def rename_symmetric_atoms(
+    residue_type: Tensor,
+    atom_thin_xyz: Tensor,
+    atom_thin_mask: Tensor,
+    atom_thin_xyz_reference: Tensor,
+    eps: float = 1e-12,
+):
+    atom_thin_xyz_alt, _ = swap_ambiguous_atom_thin_atoms(
+        residue_type, atom_thin_xyz, atom_thin_mask
+    )
+
+    dist = torch.sqrt(
+        torch.sum(
+            torch.pow(
+                atom_thin_xyz[..., None, :, None, :]
+                - atom_thin_xyz[..., None, :, None, :, :],
+                2,
+            ),
+            dim=-1,
+        )
+        + 1e-12
+    )
+
+    dist_alt = torch.sqrt(
+        torch.sum(
+            torch.pow(
+                atom_thin_xyz_alt[..., None, :, None, :]
+                - atom_thin_xyz_alt[..., None, :, None, :, :],
+                2,
+            ),
+            dim=-1,
+        )
+        + 1e-12
+    )
+
+    dist_reference = torch.sqrt(
+        torch.sum(
+            torch.pow(
+                atom_thin_xyz_reference[..., None, :, None, :]
+                - atom_thin_xyz_reference[..., None, :, None, :, :],
+                2,
+            ),
+            dim=-1,
+        )
+        + 1e-12
+    )
+
+    lddt = torch.sqrt(eps + torch.pow((dist - dist_reference), 2))
+    lddt_alt = torch.sqrt(eps + torch.pow((dist_alt - dist_reference), 2))
+
+    atom_thin_is_ambiguous = torch.as_tensor(_make_atom_thin_is_ambiguous_mask())[
+        residue_type
+    ]
+    pair_mask = (
+        atom_thin_is_ambiguous[..., None, :, None] & atom_thin_mask[..., None, :, None]
+    ) & (
+        ~atom_thin_is_ambiguous[..., None, :, None, :]
+        & atom_thin_mask[..., None, :, None, :]
+    )
+
+    per_res_lddt = torch.sum(pair_mask * lddt, dim=(-1, -2, -3))
+    per_res_lddt_alt = torch.sum(pair_mask * lddt_alt, dim=(-1, -2, -3))
+
+    return torch.where(
+        (per_res_lddt < per_res_lddt_alt)[..., None, None],
+        atom_thin_xyz,
+        atom_thin_xyz_alt,
+    )
