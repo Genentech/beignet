42f1a2eb7b581501d34ae0f7ee0ff728c1956145
Joseph Kleinhenz <kleinhej@gene.com>
2025-05-14 20:04:45 -0400

    dockq wip

---
+++ src/beignet/structure/metrics/_dockq.py
@@ -0,0 +1,110 @@
+import einops
+from torch import Tensor
+
+from .. import ResidueArray, contact_matrix, rmsd, superimpose
+from ..selectors import (
+    AndSelector,
+    AtomNameSelector,
+    ChainSelector,
+    InterfaceResidueSelector,
+)
+
+
+def dockq_contact_score(
+    model: ResidueArray,
+    native: ResidueArray,
+    receptor_chains: list[str],
+    ligand_chains: list[str],
+) -> dict[str, Tensor]:
+    # only look at atoms present in both model and native
+    atom_thin_mask = model.atom_thin_mask & native.atom_thin_mask
+
+    native_contacts = contact_matrix(
+        native,
+        selector_A=AndSelector(ChainSelector(receptor_chains), atom_thin_mask),
+        selector_B=AndSelector(ChainSelector(ligand_chains), atom_thin_mask),
+        radius_cutoff=5.0,
+    )
+
+    model_contacts = contact_matrix(
+        model,
+        selector_A=AndSelector(ChainSelector(receptor_chains), atom_thin_mask),
+        selector_B=AndSelector(ChainSelector(ligand_chains), atom_thin_mask),
+        radius_cutoff=5.0,
+    )
+
+    shared_contacts = model_contacts & native_contacts
+    non_native_contacts = model_contacts & ~native_contacts
+
+    native_contacts = einops.reduce(native_contacts, "... l lp -> ...", "sum") // 2
+    model_contacts = einops.reduce(model_contacts, "... l lp -> ...", "sum") // 2
+    shared_contacts = einops.reduce(shared_contacts, "... l lp -> ...", "sum") // 2
+    non_native_contacts = (
+        einops.reduce(non_native_contacts, "... l lp -> ...", "sum") // 2
+    )
+
+    return {
+        "native_contacts": native_contacts,
+        "model_contacts": model_contacts,
+        "shared_contacts": shared_contacts,
+        "non_native_contacts": non_native_contacts,
+    }
+
+
+def dockq_irmsd_score(
+    model: "ResidueArray",
+    native: "ResidueArray",
+    receptor_chains: list[str],
+    ligand_chains: list[str],
+    radius_cutoff: float = 10.0,
+) -> dict[str, Tensor]:
+    _, _, interface_rmsd = superimpose(
+        native,
+        model,
+        selector=AndSelector(
+            InterfaceResidueSelector(
+                AndSelector(
+                    ChainSelector(receptor_chains),
+                    AtomNameSelector(["CA", "C", "N", "O"]),
+                ),
+                AndSelector(
+                    ChainSelector(ligand_chains),
+                    AtomNameSelector(["CA", "C", "N", "O"]),
+                ),
+                radius_cutoff=radius_cutoff,
+            ),
+            AtomNameSelector(["CA", "C", "N", "O"]),
+        ),
+        rename_symmetric_atoms=False,
+    )
+
+    return {"interface_rmsd": interface_rmsd}
+
+
+def dockq_lrmsd_score(
+    model: "ResidueArray",
+    native: "ResidueArray",
+    receptor_chains: list[str],
+    ligand_chains: list[str],
+) -> dict[str, Tensor]:
+    # align on receptor
+    model, _, receptor_rmsd = superimpose(
+        native,
+        model,
+        selector=AndSelector(
+            ChainSelector(receptor_chains),
+            AtomNameSelector(["CA", "C", "N", "O"]),
+        ),
+    )
+
+    # rmsd on ligand
+    ligand_rmsd = rmsd(
+        model,
+        native,
+        selector=AndSelector(
+            ChainSelector(ligand_chains),
+            AtomNameSelector(["CA", "C", "N", "O"]),
+        ),
+    )
+
+    return {"receptor_rmsd": receptor_rmsd, "ligand_rmsd": ligand_rmsd}
---
+++ tests/beignet/structure/metrics/test__dockq.py
@@ -0,0 +1,135 @@
+import json
+import pathlib
+from pprint import pprint
+
+import pytest
+import torch
+
+from beignet.structure import ResidueArray
+from beignet.structure.metrics import (
+    dockq_contact_score,
+    dockq_irmsd_score,
+    dockq_lrmsd_score,
+)
+from beignet.structure.selectors import ChainSelector
+
+
+@pytest.fixture
+def dockq_test_data_path():
+    return pathlib.Path(__file__).parent / "data"
+
+
+def test_dockq_contact_score(dockq_test_data_path):
+    native = ResidueArray.from_pdb(dockq_test_data_path / "1A2K_r_l_b.pdb")
+    native = torch.cat(
+        [
+            native[ChainSelector(["A"])],
+            native[ChainSelector(["B"])],
+            native[ChainSelector(["C"])],
+        ]
+    )
+
+    # reorder chains to match
+    model = ResidueArray.from_pdb(dockq_test_data_path / "1A2K_r_l_b.model.pdb")
+    model = torch.cat(
+        [
+            model[ChainSelector(["B"])].rename_chains({"B": "A"}),
+            model[ChainSelector(["A"])].rename_chains({"A": "B"}),
+            model[ChainSelector(["C"])],
+        ]
+    )
+
+    with open(dockq_test_data_path / "dockq_ref.json", "r") as f:
+        ref = json.load(f)
+
+    pprint(ref["best_result"]["BC"])
+
+    results = dockq_contact_score(
+        model, native, receptor_chains=["B"], ligand_chains=["C"]
+    )
+
+    pprint(results)
+
+    assert results["model_contacts"].item() == ref["best_result"]["BC"]["model_total"]
+    assert results["native_contacts"].item() == ref["best_result"]["BC"]["nat_total"]
+    assert results["shared_contacts"].item() == ref["best_result"]["BC"]["nat_correct"]
+    assert (
+        results["non_native_contacts"].item()
+        == ref["best_result"]["BC"]["nonnat_count"]
+    )
+
+
+def test_dockq_irmsd_score(dockq_test_data_path):
+    native = ResidueArray.from_pdb(
+        dockq_test_data_path / "1A2K_r_l_b.pdb", dtype=torch.float64
+    )
+    native = torch.cat(
+        [
+            native[ChainSelector(["A"])],
+            native[ChainSelector(["B"])],
+            native[ChainSelector(["C"])],
+        ]
+    )
+
+    # reorder chains to match
+    model = ResidueArray.from_pdb(
+        dockq_test_data_path / "1A2K_r_l_b.model.pdb", dtype=torch.float64
+    )
+    model = torch.cat(
+        [
+            model[ChainSelector(["B"])].rename_chains({"B": "A"}),
+            model[ChainSelector(["A"])].rename_chains({"A": "B"}),
+            model[ChainSelector(["C"])],
+        ]
+    )
+
+    with open(dockq_test_data_path / "dockq_ref.json", "r") as f:
+        ref = json.load(f)
+
+    pprint(ref["best_result"]["BC"])
+
+    irmsd = dockq_irmsd_score(
+        model, native, receptor_chains=["B"], ligand_chains=["C"]
+    )["interface_rmsd"]
+
+    pprint(f"{irmsd.item()=:0.2e}")
+
+    assert irmsd.item() == pytest.approx(ref["best_result"]["BC"]["iRMSD"], abs=1e-5)
+
+
+def test_dockq_lrmsd_score(dockq_test_data_path):
+    native = ResidueArray.from_pdb(
+        dockq_test_data_path / "1A2K_r_l_b.pdb", dtype=torch.float64
+    )
+    native = torch.cat(
+        [
+            native[ChainSelector(["A"])],
+            native[ChainSelector(["B"])],
+            native[ChainSelector(["C"])],
+        ]
+    )
+
+    # reorder chains to match
+    model = ResidueArray.from_pdb(
+        dockq_test_data_path / "1A2K_r_l_b.model.pdb", dtype=torch.float64
+    )
+    model = torch.cat(
+        [
+            model[ChainSelector(["B"])].rename_chains({"B": "A"}),
+            model[ChainSelector(["A"])].rename_chains({"A": "B"}),
+            model[ChainSelector(["C"])],
+        ]
+    )
+
+    with open(dockq_test_data_path / "dockq_ref.json", "r") as f:
+        ref = json.load(f)
+
+    pprint(ref["best_result"]["BC"])
+
+    lrmsd = dockq_lrmsd_score(
+        model, native, receptor_chains=["C"], ligand_chains=["B"]
+    )["ligand_rmsd"]
+
+    pprint(f"{lrmsd.item()=:0.2e}")
+
+    assert lrmsd.item() == pytest.approx(ref["best_result"]["BC"]["LRMSD"], abs=1e-3)
