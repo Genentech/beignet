3878cb366c20adc5e2686636da0f74c7e006598b
Joseph Kleinhenz <kleinhej@gene.com>
2025-05-18 01:32:45 -0400

    wip bbt_to_atom_thin

--- tests/beignet/structure/test__frames.py
+++ tests/beignet/structure/test__frames.py
@@ -1,11 +1,14 @@
+import dataclasses
+
 import torch
 
-from beignet.structure import ResidueArray
+from beignet.structure import ResidueArray, rmsd
 from beignet.structure._frames import (
     backbone_coordinates_to_frames,
     backbone_frames_to_coordinates,
-    bbt_to_global_frames,
+    bbt_to_atom_thin,
 )
+from beignet.structure.selectors import ChainSelector, PeptideBackboneSelector
 
 
 def test_backbone_coordinates_to_frames(structure_7k7r_pdb):
@@ -38,12 +41,25 @@ def test_backbone_coordinates_to_frames(structure_7k7r_pdb):
 
 def test_bbt(structure_7k7r_pdb):
     p = ResidueArray.from_pdb(structure_7k7r_pdb)
+    p = p[ChainSelector(["A"])]
 
     bb_frames, bb_mask = p.backbone_frames
     torsions, torsions_mask = p.torsions
 
-    global_frames, global_frames_mask = bbt_to_global_frames(
+    atom_thin_xyz, atom_thin_mask = bbt_to_atom_thin(
         bb_frames, bb_mask, torsions, torsions_mask, p.residue_type
     )
 
-    print(f"{global_frames.shape=}")
+    p_ideal = dataclasses.replace(
+        p, atom_thin_xyz=atom_thin_xyz, atom_thin_mask=atom_thin_mask
+    )
+
+    rmsd_val = rmsd(p, p_ideal)
+
+    bb_rmsd_val = rmsd(p, p_ideal, selector=PeptideBackboneSelector())
+
+    print(f"{bb_rmsd_val=}")
+    print(f"{rmsd_val=}")
+
+    p.to_pdb("7k7r.pdb")
+    p_ideal.to_pdb("7k7r_ideal.pdb")
