86bac82be4d33b68ca1b8dff195e07c0d72d4750
Joseph Kleinhenz <kleinhej@gene.com>
2025-06-24 00:07:54 +0000

    typo

--- src/beignet/_canonicalize_quaternion.py
+++ src/beignet/_canonicalize_quaternion.py
@@ -15,7 +15,7 @@ def canonicalize_quaternion(input: Tensor):
     output: Tensor, shape=(...,4)
         Canonicalized quaternion
 
-    The caonicalized quaternion is chosen from :math:`{q, -q}`
+    The canonicalized quaternion is chosen from :math:`{q, -q}`
     such that the :math:`w` term is positive.
     If the :math:`w` term is :math:`0`, then the rotation quaternion is
     chosen such that the first non-zero term of the :math:`x`, :math:`y`,
